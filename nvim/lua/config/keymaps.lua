d2d21d779c7884537768c06646729d5437b705ea
kia <kiasn21@gmail.com>
2024-02-01 23:16:04 +0330

    update: lazy neovim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+--local Util = require("lazyvim.util")
+
+---local map = vim.keymap.set
+--map("t", "<C-h>", "<cmd>ToggleTerm direction=float<cr>", { desc = "Go to left window" })
+
+-- lvim.keys.normal_mode["<C-t>"] = ":ToggleTermToggleAll<CR>"
